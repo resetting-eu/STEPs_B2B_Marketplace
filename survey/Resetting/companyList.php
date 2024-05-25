64d8469d621dc7d27ef7690b9e983399da9011f7
Carlos Coutinho <cedco@iscte-iul.pt>
2024-05-25 01:17:33 +0000

    .

--- survey/Resetting/companyList.php
+++ survey/Resetting/companyList.php
@@ -3,6 +3,33 @@ include "iscte_utils.php";
 
 session_start();
 
+function populateCompaniesCombo($loginID) {
+    // Connect to the Database
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
+    if ($conn->connect_errno) {
+        iscte_error("Failed to connect to the database: ".$conn->connect_error);
+        $errorMsg = "Failed to connect to the database: ".$conn->connect_error;
+        exit();
+    }
+
+    // Get the companies (if any) of this loginID ==> result
+    $sql = "SELECT Company.CompanyID, CompanyName FROM Company INNER JOIN CompanyLogin INNER JOIN Login WHERE Login.LoginID = CompanyLogin.LoginID AND Company.CompanyID = CompanyLogin.CompanyID AND Login.LoginID=$loginID ORDER BY CompanyName";
+    iscte_debug("sql:$sql");
+    $result = $conn->query($sql);
+    // Close the connection to the database
+    $conn->close();
+
+    // Check database results
+    iscte_debug("result->num_rows:$result->num_rows");
+    if ($result->num_rows > 0) {
+        // Loop all results and create the combobox options
+        while ($row = $result->fetch_assoc()) {
+            echo '<li id="'.$row["CompanyID"].'" role="option">'.$row["CompanyName"].'</li>';
+        }
+    }
+    $result->free_result(); // Free result set
+}
+
 // Get the information from the HTML form's POST action
 iscte_debug("_SERVER[REQUEST_METHOD]:".$_SERVER["REQUEST_METHOD"]);
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
@@ -15,41 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $loginID = $_SESSION["LoginID"];
     $name = $_SESSION["Name"];
     iscte_debug("loginID:$loginID; name:$name");
-
-    // Connect to the Database
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
-    if ($conn->connect_errno) {
-        iscte_error("Failed to connect to the database: ".$conn->connect_error);
-        $errorMsg = "Failed to connect to the database: ".$conn->connect_error;
-        exit();
-    }
-
-    // Get the companies (if any) of this loginID ==> result
-    $sql = "SELECT Company.CompanyID, CompanyName FROM Company INNER JOIN CompanyLogin INNER JOIN Login WHERE Login.LoginID = CompanyLogin.LoginID AND Company.CompanyID = CompanyLogin.CompanyID AND Login.LoginID=$loginID";
-    iscte_debug("sql:$sql");
-    $result = $conn->query($sql);
-    // Close the connection to the database
-    $conn->close();
-
-    $companies = [];    // Array with company IDs and names
-
-    // Check database results
-    iscte_debug("result->num_rows:$result->num_rows");
-    if ($result->num_rows > 0) {
-        // Loop all results and store them in the companies array
-        while ($row = $result->fetch_assoc()) {
-            $companies[] = [
-                "id" => $row["CompanyID"],
-                "name" => $row["CompanyName"]
-            ];
-        }
-    }
-    $result->free_result(); // Free result set
-    // Return the data as an Encoded JSON text
-    header('Content-Type: application/json');
-    echo json_encode($companies);
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -78,61 +71,56 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 <body>
     <div class="border">
         <h2>Welcome, <?php echo $_SESSION['Name']; ?></h2>
-        <h3>Please choose the company that you want to work with</h3>
+        <h3>Please select the company you want to work with.</h3>
         <label for="company_select">Company name:</label>
-        <!-- <div class="combobox combobox-list">
-        <div class="group">
-            <input id="company_select" class="cb_edit" type="text" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-controls="cb1-listbox">
-            <button id="cb1-button" tabindex="-1" aria-label="States" aria-expanded="false" aria-controls="cb1-listbox">
-            <svg width="18" height="16" aria-hidden="true" focusable="false" style="forced-color-adjust: auto">
-                <polygon class="arrow" stroke-width="0" fill-opacity="0.75" fill="currentcolor" points="3,6 15,6 9,14"></polygon>
-            </svg>
-            </button>
-        </div>
-        <ul id="cb1-listbox" role="listbox" aria-label="Companies">
-            <li id="lb1-al" role="option">Alabama</li>
-            <li id="lb1-ak" role="option">Alaska</li>
-            <li id="lb1-as" role="option">American Samoa</li>
-        </ul> -->
-
-        <div class="custom-dropdown">
-            <input list="companies" id="company_select" name="company_select" oninput="toggleButtons()">
-            <datalist id="companies"></datalist>
+        <div class="combobox combobox-list">
+            <div class="group">
+                <input id="company_select" class="cb_edit" type="text" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-controls="cb1-listbox">
+                <button id="cb1-button" tabindex="-1" aria-label="States" aria-expanded="false" aria-controls="cb1-listbox">
+                <svg width="18" height="16" aria-hidden="true" focusable="false" style="forced-color-adjust: auto">
+                    <polygon class="arrow" stroke-width="0" fill-opacity="0.75" fill="currentcolor" points="3,6 15,6 9,14"></polygon>
+                </svg>
+                </button>
+            </div>
+            <ul id="cb1-listbox" role="listbox" aria-label="Companies">
+                <?php populateCompaniesCombo($loginID); ?>
+            </ul>
         </div>
         <br><br>
         <button id="edit_button" onclick="editCompany()" style="display:none;">Edit Company</button>
         <button id="create_company" onclick="createCompany()">Create a New Company</button>
     </div>
 
+    <script type="text/javascript" src="scripts/editable-combobox.js"></script>
     <script>
-        // Função para carregar as companhias no datalist usando AJAX
-        function loadCompanies() {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'companyList.php', true);
+        // // Função para carregar as companhias no datalist usando AJAX
+        // function loadCompanies() {
+        //     var xhr = new XMLHttpRequest();
+        //     xhr.open('GET', 'companyList.php', true);
 
-            xhr.onload = function () {
-                if (xhr.status == 200) {
-                    var companies = JSON.parse(xhr.responseText);
-                    var datalist = document.getElementById('companies');
+        //     xhr.onload = function () {
+        //         if (xhr.status == 200) {
+        //             var companies = JSON.parse(xhr.responseText);
+        //             var datalist = document.getElementById('companies');
 
-                    console.log("@DEBUG: 1")
-                    // Adicionar as opções do datalist com base nos dados retornados
-                    companies.forEach(function (company) {
-                        console.log("@DEBUG: 2")
-                        var option = document.createElement('option');
-                        option.value = company.name;
-                        datalist.appendChild(option);
-                    });
-                } else {
-                    console.error('Failed to load companies.');
-                }
-            };
+        //             console.log("@DEBUG: 1")
+        //             // Adicionar as opções do datalist com base nos dados retornados
+        //             companies.forEach(function (company) {
+        //                 console.log("@DEBUG: 2")
+        //                 var option = document.createElement('option');
+        //                 option.value = company.name;
+        //                 datalist.appendChild(option);
+        //             });
+        //         } else {
+        //             console.error('Failed to load companies.');
+        //         }
+        //     };
 
-            xhr.send();
-        }
+        //     xhr.send();
+        // }
 
-        // Chamada para carregar as companhias ao carregar a página
-        loadCompanies();
+        // // Chamada para carregar as companhias ao carregar a página
+        // loadCompanies();
 
         // Função para lidar com o botão "Edit"
         function editCompany() {
