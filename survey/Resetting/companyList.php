18f68988bb9d0045908eb612d00267dbe582a785
Carlos Coutinho <cedco@iscte-iul.pt>
2024-05-27 08:58:41 +0000

    .

--- survey/Resetting/companyList.php
+++ survey/Resetting/companyList.php
@@ -14,7 +14,9 @@ function populateCompaniesCombo() {
         exit();
     }
 
-    // Get the companies (if any) of this loginID ==> result
+    /**
+     * Get the companies (if any) of this loginID ==> result
+     */
     $sql = "SELECT Company.CompanyID, CompanyName FROM Company INNER JOIN CompanyLogin INNER JOIN Login WHERE Login.LoginID = CompanyLogin.LoginID AND Company.CompanyID = CompanyLogin.CompanyID AND Login.LoginID=$loginID ORDER BY CompanyName";
     iscte_debug("sql:$sql");
     $result = $conn->query($sql);
@@ -36,6 +38,10 @@ $loginID = $_SESSION["LoginID"];
 // Get the information from the HTML form's POST action
 iscte_debug("_SERVER[REQUEST_METHOD]:".$_SERVER["REQUEST_METHOD"]);
 if ("GET" == $_SERVER["REQUEST_METHOD"]) {
+    /**
+     * This is the first call, when the form has not been submitted yet.
+     */
+
     // Ensure that to reach this page the user has registered or login successfully
     if (!isset($_SESSION["LoginID"])) {
         iscte_error("This form is only accessible by a logged-in user");
@@ -45,8 +51,12 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
     $name = $_SESSION["Name"];
     iscte_debug("loginID:$loginID; name:$name");
 } else if ("POST" == $_SERVER["REQUEST_METHOD"]) {
+    /**
+     * This is the call, AFTER the form has been submitted (POST).
+     */
+
     iscte_debugPostFields();
-    $companyName = $_POST["companyName"];
+    $companyName = $_POST["CompanyName"];
 
     // Connect to the Database
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
@@ -56,6 +66,77 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
         exit();
     }
 
+    /**
+     * Do a first query: Get the companies (if any) with this CompanyName ==> result. This tells us if:
+     *  - 1) The selected CompanyName is already exists on the database, or
+     *  - 2) The selected CompanyName is new.
+     */
+    $sql = "SELECT * FROM Company WHERE CompanyName = $companyName";
+    iscte_debug("sql:$sql");
+    $result = $conn->query($sql);
+    // Close the connection to the database
+    $conn->close();
+
+    /**
+     * Check database results:
+     *  - If there is (at least) one row (it should be at most only one), this means we are on case 1) The selected CompanyName is already exists on the database.
+     *  - If there are no rows, this means we are on case 2) The selected CompanyName is new.
+     */
+    iscte_debug("result->num_rows:$result->num_rows");
+    if ($result->num_rows > 0) {
+        /**
+         * 1) The selected CompanyName is already exists on the database? This can happen if:
+         *  - 1.1) The user is editing HIS company.
+         *  - 1.2) The user is inserting a NEW company with a name which already exists.
+         */
+        $result->free_result(); // Free result set
+
+        /**
+         * Do a second query: Get the companies (if any) with this CompanyName AND with this LoginID ==> result. This tells us if:
+         *  - 1.1) The user is editing HIS company
+         *  - 1.2) The user is inserting a NEW company with a name which already exists.
+         */
+        $sql = "SELECT Company.CompanyID FROM Company INNER JOIN CompanyLogin INNER JOIN Login WHERE Login.LoginID = CompanyLogin.LoginID AND Company.CompanyID = CompanyLogin.CompanyID AND Company.CompanyName = $companyName AND Login.LoginID = $loginID";
+        iscte_debug("sql:$sql");
+        $result = $conn->query($sql);
+        // Close the connection to the database
+        $conn->close();
+
+        /**
+         * Check database results:
+         *  - If there is (at least) one row (it should be at most only one), this means we are on case 1.1) The user is editing HIS company.
+         *  - If there are no rows, this means we are on case 1.2) The user is inserting a NEW company with a name which already exists.
+         */
+        iscte_debug("result->num_rows:$result->num_rows");
+        if (1 == $result->num_rows) {
+            /**
+             * 1.1) The user is editing HIS company (OK): Do the following actions:
+             *  - 1.1.1) Store CompanyID in the session.
+             *  - 1.1.2) Proceed to Edit the company with CompanyID.
+             */
+            $row = $result->fetch_assoc();
+            $result->free_result(); // Free result set
+            $_SESSION["CompanyID"] = $row["CompanyID"];
+        } else {
+            /**
+             * 1.2) The user is inserting a NEW company with a name which already exists (NOK): State Error and return to the form.
+             */
+            $result->free_result(); // Free result set
+            iscte_error("The selected Company Name is already in use. Please try again.");
+            $errorMsg = "The selected Company Name is already in use. Please try again.";
+            exit();
+        }
+    } else {
+        /**
+         * 2) The selected CompanyName is new (OK): Do the following actions:
+         *  - 2.1) Create a new Company with this CompanyName.
+         *  - 2.2) Get the CompanyID of the new Company (fill in session).
+         *  - 2.3) Create a new CompanyLogin with the CompanyID and the LoginID.
+         *  - 2.4) Proceed to Edit the company with CompanyID.
+         */
+        $result->free_result(); // Free result set
+
+    }
     // Get the companies (if any) of this loginID ==> result
     $sql = "SELECT Login.LoginID, Company.CompanyID FROM Company INNER JOIN CompanyLogin INNER JOIN Login WHERE Login.LoginID = CompanyLogin.LoginID AND Company.CompanyID = CompanyLogin.CompanyID AND Company.CompanyName = $companyName";
     iscte_debug("sql:$sql");
@@ -65,7 +146,7 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
 
     // Check database results
     iscte_debug("result->num_rows:$result->num_rows");
-    if ($result->num_rows > 0) {    // companyName already exists in the database?
+    if ($result->num_rows > 0) {    // CompanyName already exists in the database?
         // Loop all results, searching for my LoginID as
         while ($row = $result->fetch_assoc()) {
             echo '<li id="'.$row["CompanyID"].'" role="option">'.$row["CompanyName"].'</li>';
@@ -105,7 +186,7 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
         function updateValue() {
             let combobox = document.getElementById("company_select");
             var companyName = combobox.value;
-            document.getElementById("companyName").value = companyName;
+            document.getElementById("CompanyName").value = companyName;
             console.log("@DEBUG: [textoCombo:" + companyName + "]");
             let optionList = document.getElementById('cb1-listbox').getElementsByTagName('LI');
             var editExistingCompany = false;
@@ -143,7 +224,7 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
         <div class="border">
             <div style="color: red"><?php echo $errorMsg ?></div>
             <h2>Resetting Login</h2>
-            <input type="hidden" id="companyName" name="companyName">
+            <input type="hidden" id="CompanyName" name="CompanyName">
             <label for="company_select">Company name:</label>
             <div class="combobox combobox-list">
                 <div class="group">
@@ -162,32 +243,5 @@ if ("GET" == $_SERVER["REQUEST_METHOD"]) {
             <button id="create-button" type="submit" disabled="disabled" style="background:grey";>Select Company / Create New Company</button>
         </div>
     </form>
-
-    <!-- <form method="post" onsubmit="javascript: return updateValue();">
-        <div class="border">
-            <div style="color: red"><?php echo $errorMsg ?></div>
-            <h2>Welcome, <?php echo $_SESSION['Name']; ?></h2>
-            <h3>Please select the company you want to edit.</h3>
-            <input type="text" id="companyName">
-            <input type="text" id="companyName1" value="test">
-            <label for="company_select">Company name:</label>
-            <!- <div class="combobox combobox-list">
-                <div class="group">
-                    <input id="company_select" class="cb_edit" type="text" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-controls="cb1-listbox" oninput="updateValue()">
-                    <button type="button" id="cb1-button" aria-label="Companies" aria-expanded="false" aria-controls="cb1-listbox" tabindex="-1">
-                    <svg width="18" height="16" aria-hidden="true" focusable="false" style="forced-color-adjust: auto">
-                        <polygon class="arrow" stroke-width="0" fill-opacity="0.75" fill="currentcolor" points="3,6 15,6 9,14"></polygon>
-                    </svg>
-                    </button>
-                </div>
-                <ul id="cb1-listbox" role="listbox" aria-label="Companies">
-                    <?php populateCompaniesCombo(); ?>
-                </ul>
-            </div> --
-            <br><br>
-            <button id="create-button" type="submit" style="background:grey";>Select Company / Create New Company</button>
-        </div>
-    </form> -->
-
 </body>
 </html>
