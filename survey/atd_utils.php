a138fbc0f81a060f4cf828de309c84620b1e5d10
Carlos Coutinho <carlos.coutinho@caixamagica.pt>
2025-01-29 22:06:39 +0000

    Updated iscte to atd

--- survey/atd_utils.php
+++ survey/atd_utils.php
@@ -1,5 +1,5 @@
 <?php
-define("ISCTE_DEBUG", true);
+define("ATD_DEBUG", true);
 
 // Connection string to the database
 define("DB_HOST", "localhost");
@@ -11,8 +11,8 @@ define("DB_DATABASE", "resetting_survey_dev");
  * @brief Prints a debug message in the Javascript Console (On Google Chrome, press F12 to see the console). Shows the file and line of code besides the message.
  * @param msg Text message to be printed
  */
-function iscte_debug ($msg) {
-    if (ISCTE_DEBUG) {
+function atd_debug ($msg) {
+    if (ATD_DEBUG) {
         $debugTrace = debug_backtrace();
         echo "<script>console.log('@DEBUG {".basename(print_r($debugTrace[0]["file"], true)).":".print_r($debugTrace[0]["line"], true)."}: [$msg]');</script>";
     }
@@ -22,21 +22,21 @@ function iscte_debug ($msg) {
  * @brief Prints a debug message in an alert Javascript window. Shows the file and line of code besides the message. Useful to stop the processing and alert.
  * @param msg Text message to be printed
  */
-function iscte_debugAlert ($msg) {
-    if (ISCTE_DEBUG) {
+function atd_debugAlert ($msg) {
+    if (ATD_DEBUG) {
         $debugTrace = debug_backtrace();
         echo "<script>alert('@DEBUG {".basename(print_r($debugTrace[0]["file"], true)).":".print_r($debugTrace[0]["line"], true)."}: [$msg]');</script>";
    }
 }
 
 /**
- * @brief Calls iscte_debug to print the message, but then exits the page. This is useful to debug the info just BEFORE jumping to a next page.
+ * @brief Calls atd_debug to print the message, but then exits the page. This is useful to debug the info just BEFORE jumping to a next page.
  * @param msg Text message to be printed
  */
-function iscte_debugAndExit ($msg) {
-    if (ISCTE_DEBUG) {
-        iscte_debug($msg);
-        iscte_debug("@@ EXIT @@");
+function atd_debugAndExit ($msg) {
+    if (ATD_DEBUG) {
+        atd_debug($msg);
+        atd_debug("@@ EXIT @@");
         exit();   // This prevents going to the next page (so we can still see the information on the previous one)
     }
 }
@@ -46,47 +46,47 @@ function iscte_debugAndExit ($msg) {
  * @param assocArray the associative array
  * @param assocArrayName the name of the associative array (for debug purposes)
  */
-function iscte_debugAssocArrayFields ($assocArray, $assocArrayName) {
-    iscte_debug("Stt Printing: $assocArrayName: [ key => value ]");
+function atd_debugAssocArrayFields ($assocArray, $assocArrayName) {
+    atd_debug("Stt Printing: $assocArrayName: [ key => value ]");
     foreach($assocArray as $key => $value)
-        iscte_debug("$assocArrayName: [ $key => $value ]");
-    iscte_debug("End Printing: $assocArrayName: [ key => value ]");
+        atd_debug("$assocArrayName: [ $key => $value ]");
+    atd_debug("End Printing: $assocArrayName: [ key => value ]");
 }
 
 /**
  * @brief Prints all $_SESSION fields on the current page.
  */
-function iscte_debugSessionFields () {
-    iscte_debugAssocArrayFields($_SESSION, "_SESSION");
+function atd_debugSessionFields () {
+    atd_debugAssocArrayFields($_SESSION, "_SESSION");
 }
 
 /**
  * @brief Prints all $_POST fields on the current page.
  */
-function iscte_debugPostFields () {
-    iscte_debugAssocArrayFields($_POST, "_POST");
+function atd_debugPostFields () {
+    atd_debugAssocArrayFields($_POST, "_POST");
 }
 
 /**
  * @brief Prints all $_GET fields on the current page.
  */
-function iscte_debugGetFields () {
-    iscte_debugAssocArrayFields($_GET, "_GET");
+function atd_debugGetFields () {
+    atd_debugAssocArrayFields($_GET, "_GET");
 }
 
 /**
  * @brief Prints all $_SERVER fields on the current page.
  */
-function iscte_debugServerFields () {
-    iscte_debugAssocArrayFields($_SERVER, "_SERVER");
+function atd_debugServerFields () {
+    atd_debugAssocArrayFields($_SERVER, "_SERVER");
 }
 
 /**
  * @brief Prints an error message in the Javascript Console (On Google Chrome, press F12 to see the console). Shows the file and line of code besides the message.
  * @param msg Text message to be printed
  */
-function iscte_error ($msg) {
-    if (ISCTE_DEBUG) {
+function atd_error ($msg) {
+    if (ATD_DEBUG) {
         $debugTrace = debug_backtrace();
        echo "<script>console.log('@ERROR {".basename(print_r($debugTrace[0]["file"], true)).":".print_r($debugTrace[0]["line"], true)."}: [$msg]');</script>";
     }
@@ -96,8 +96,8 @@ function iscte_error ($msg) {
  * @brief Prints an error message in an alert Javascript window. Shows the file and line of code besides the message. Useful to stop the processing and alert.
  * @param msg Text message to be printed
  */
-function iscte_errorAlert ($msg) {
-    if (ISCTE_DEBUG) {
+function atd_errorAlert ($msg) {
+    if (ATD_DEBUG) {
         $debugTrace = debug_backtrace();
         echo "<script>alert('@ERROR {".basename(print_r($debugTrace[0]["file"], true)).":".print_r($debugTrace[0]["line"], true)."}: [$msg]');</script>";
     }
--- survey/register.php
+++ survey/register.php
@@ -1,25 +1,26 @@
 <?php
-include "iscte_utils.php";
+include "atd_utils.php";
 
 session_start();
 // session_destroy(); session_start();  // @DEBUG: If you wish to clear all PHP session variables, uncomment this statement
-iscte_debugSessionFields();
+atd_debugSessionFields();
 
 // Get the information from the HTML form's POST action
-iscte_debug("_SERVER[REQUEST_METHOD]:".$_SERVER["REQUEST_METHOD"]);
+atd_debug("_SERVER[REQUEST_METHOD]:".$_SERVER["REQUEST_METHOD"]);
+$errorMsg = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
-    iscte_debugPostFields();
-    $name = $_POST["Name"]; iscte_debug("name:$name");
-    $email = $_POST["Email"]; iscte_debug("email:$email");
-    $password = $_POST["Password"]; iscte_debug("password:$password");
-    $description = $_POST["Description"]; iscte_debug("description:$description");
+    atd_debugPostFields();
+    $name = $_POST["Name"]; atd_debug("name:$name");
+    $email = $_POST["Email"]; atd_debug("email:$email");
+    $password = $_POST["Password"]; atd_debug("password:$password");
+    $description = $_POST["Description"]; atd_debug("description:$description");
     // Hashed password (recommended for increased security)
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT); iscte_debug("passwordHash:$passwordHash");
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT); atd_debug("passwordHash:$passwordHash");
 
     // Connect to the Database
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
     if ($mysqli->connect_errno) {
-        iscte_error("Failed to connect to the database: ".$mysqli->connect_error);
+        atd_error("Failed to connect to the database: ".$mysqli->connect_error);
         $errorMsg = "Failed to connect to the database: ".$mysqli->connect_error;
         exit();
     }
@@ -27,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
     /**
      * Get all Login elements with the inserted Email ==> dbResult
      */
-    $dbSQL = "SELECT * FROM Login WHERE Email = \"$email\""; iscte_debug("dbSQL:$dbSQL");
+    $dbSQL = "SELECT * FROM Login WHERE Email = \"$email\""; atd_debug("dbSQL:$dbSQL");
     $dbResult = $mysqli->query($dbSQL);
 
     /**
@@ -35,11 +36,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
      * * =0: (OK) Not found
      * * >0: (NOK) There is already another user with the same email
      */
-    iscte_debug("dbResult->num_rows:$dbResult->num_rows");
+    atd_debug("dbResult->num_rows:$dbResult->num_rows");
     if ($dbResult->num_rows > 0) {
         $dbResult->free_result(); // Free dbResult set
         // * >0: (NOK) There is already another user with the same email
-        iscte_error("A user with the inserted e-mail already exists.<BR>Please try again.");
+        atd_error("A user with the inserted e-mail already exists.<BR>Please try again.");
         $errorMsg = "A user with the inserted e-mail already exists.<BR>Please try again.";
     } else {
         $dbResult->free_result(); // Free dbResult set
@@ -48,15 +49,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
         /**
          * Insert the new user in the database
          */
-        $dbSQL = "INSERT INTO Login (Name, Email, Password, Description) VALUES (\"$name\", \"$email\", \"$passwordHash\", \"$description\")"; iscte_debug("dbSQL:$dbSQL");
+        $dbSQL = "INSERT INTO Login (Name, Email, Password, Description) VALUES (\"$name\", \"$email\", \"$passwordHash\", \"$description\")"; atd_debug("dbSQL:$dbSQL");
         if (!$mysqli->query($dbSQL)) { // If operation was NOT successful
-            iscte_error("Error inserting new user in database. Please try again.");
+            atd_error("Error inserting new user in database. Please try again.");
             $errorMsg = "Error inserting new user in database. Please try again.";
         } else {
             /**
              * Get the generated LoginID of the element that was inserted ==> dbResult
              */
-            $dbSQL = "SELECT * FROM Login WHERE Email = \"$email\""; iscte_debug("dbSQL:$dbSQL");
+            $dbSQL = "SELECT * FROM Login WHERE Email = \"$email\""; atd_debug("dbSQL:$dbSQL");
             $dbResult = $mysqli->query($dbSQL);
             $mysqli->close(); // Close the connection to the database
 
@@ -66,26 +67,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
              * * =1: (OK) It should return only 1 (one) row
              * * >1: (NOK) There can only be one user with the same email
              */
-            iscte_debug("dbResult->num_rows:$dbResult->num_rows");
+            atd_debug("dbResult->num_rows:$dbResult->num_rows");
             if (1 != $dbResult->num_rows) {
                 $dbResult->free_result(); // Free dbResult set
                 // * =0: (NOK) Not found
                 // * >1: (NOK) There can only be one user with the same email
-                iscte_error("User not inserted. Please try again.");
+                atd_error("User not inserted. Please try again.");
                 $errorMsg = "User not inserted. Please try again.";
             } else {
                 // * =1: (OK) It should return only 1 (one) row
                 $user = $dbResult->fetch_assoc();
                 $dbResult->free_result(); // Free dbResult set
-                iscte_debug("user.Email:".$user["Email"]."; user.LoginID:".$user["LoginID"]);
+                atd_debug("user.Email:".$user["Email"]."; user.LoginID:".$user["LoginID"]);
 
                 // Store Login Info in session
                 $_SESSION["LoginID"] = $user["LoginID"];
                 $_SESSION["Name"] = $user["Name"];
-                iscte_debugSessionFields();
+                atd_debugSessionFields();
 
                 // If credentials are OK, jump to next page
-                // iscte_debugAndExit("Jumping to companyList.php");    // @DEBUG: If you need debug info BEFORE jumping to the next page, uncomment this statement
+                // atd_debugAndExit("Jumping to companyList.php");    // @DEBUG: If you need debug info BEFORE jumping to the next page, uncomment this statement
                 header("Location: companyList.php");
                 exit();
             }
@@ -97,63 +98,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Email"])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Resetting</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .border {
-            max-width: 400px;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        h2 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-        input[type="text"],
-        input[type="email"],
-        input[type="password"],
-        textarea {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
-        button[type="submit"] {
-            width: 100%;
-            background-color: #007bff;
-            color: #fff;
-            padding: 10px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            text-align: center;
-            display: block;
-            text-decoration: none;
-        }
-        button[type="submit"]:hover {
-            background-color: #0056b3;
-        }
-    </style>
 </head>
 <body>
     <form method="post">
